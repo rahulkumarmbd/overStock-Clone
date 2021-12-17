ff545e7a57c31ed8012d42c0e641905fdb394864
nitesh172 <niteshbaghel172@gmail.com>
2021-12-17 18:22:21 +0530

    Update

--- Script/category.js
+++ Script/category.js
@@ -1,96 +1,107 @@
-var pageName = localStorage.getItem("pageName") || "";
-var nn = pageName;
-console.log(nn);
+var pageName = localStorage.getItem("pageName") || ""
+var nn = pageName
+console.log(nn)
 fetch(`https://overstockapi.herokuapp.com/page/${pageName}`)
   .then((response) => {
-    return response.json();
+    return response.json()
   })
   .then((response) => {
-    var data = response;
-    console.log(data);
-    document.getElementById("bimg").setAttribute("src", data.imgURL);
-    document.getElementById("bimg1").setAttribute("src", data.imgURL);
-    document.getElementById("pageName").textContent = data.pageName;
-    document.getElementById(
-      "offerCat"
-    ).textContent = `Select ${data.pageName}*`;
+    var data = response
+    console.log(data)
+    document.getElementById("bimg").setAttribute("src", data.imgURL)
+    document.getElementById("bimg1").setAttribute("src", data.imgURL)
+    document.getElementById("pageName").textContent = data.pageName
+    document.getElementById("offerCat").textContent = `Select ${data.pageName}*`
     document.getElementById(
       "heading2"
-    ).textContent = `Popular ${data.pageName} Catergories`;
+    ).textContent = `Popular ${data.pageName} Catergories`
     document.getElementById(
       "moreHeading"
-    ).textContent = `Shop More ${data.pageName}`;
+    ).textContent = `Shop More ${data.pageName}`
     document
       .getElementById("buyGuideImg1")
-      .setAttribute("src", data.imb1.imgUrl);
-    document.getElementById("buyGuidetitle").textContent = data.imb1.title;
-    document.getElementById("buyGuidedesc").textContent = data.imb1.desc;
+      .setAttribute("src", data.imb1.imgUrl)
+    document.getElementById("buyGuidetitle").textContent = data.imb1.title
+    document.getElementById("buyGuidedesc").textContent = data.imb1.desc
     document
       .getElementById("buyGuideImg2")
-      .setAttribute("src", data.imb2.imgUrl);
-    document.getElementById("buyGuidetitle2").textContent = data.imb1.title;
-    document.getElementById("buyGuidedesc2").textContent = data.imb1.desc;
-    document.getElementById("text1").textContent = data.text1;
-    document.getElementById("text2").textContent = data.text2;
-    document.getElementById("text3").textContent = data.text3;
-    document.getElementById("text4").textContent = data.text4;
-    document.getElementById("img1").setAttribute("src", data.img1);
-    document.getElementById("img2").setAttribute("src", data.img2);
-    appendCategories(data.catergory);
-    appendMoreCategories(data.moreCategory);
-  });
+      .setAttribute("src", data.imb2.imgUrl)
+    document.getElementById("buyGuidetitle2").textContent = data.imb1.title
+    document.getElementById("buyGuidedesc2").textContent = data.imb1.desc
+    document.getElementById("text1").textContent = data.text1
+    document.getElementById("text2").textContent = data.text2
+    document.getElementById("text3").textContent = data.text3
+    document.getElementById("text4").textContent = data.text4
+    document.getElementById("img1").setAttribute("src", data.img1)
+    document.getElementById("img2").setAttribute("src", data.img2)
+    appendCategories(data.catergory)
+    appendMoreCategories(data.moreCategory)
+  })
 
 var appendCategories = (data) => {
   data.forEach((item) => {
-    let li = document.createElement("li");
-    li.setAttribute("class", "featuredNavLi");
-    let a = document.createElement("a");
-    a.setAttribute("class", "featuredNavLiLink");
-    a.setAttribute("href", "");
-    a.textContent = item.name;
-    li.append(a);
-    document.getElementById("listCat").append(li);
-    let card = document.createElement("a");
-    card.setAttribute("href", "");
-    card.setAttribute("class", "popularCategoriesItem");
-    let imgDiv = document.createElement("div");
-    imgDiv.setAttribute("class", "popularCategoriesItemImgDiv");
-    let img = document.createElement("img");
-    img.setAttribute("src", item.imgUrl);
-    img.setAttribute("class", "popularCategoriesItemImg");
-    img.width = "400";
-    img.height = "400";
-    let titleDIv = document.createElement("div");
-    titleDIv.setAttribute("class", "popularCategoriesItemText");
-    titleDIv.textContent = item.name;
-    imgDiv.append(img);
-    card.append(imgDiv, titleDIv);
-    document.getElementById("catWithImg").append(card);
-  });
-};
+    let li = document.createElement("li")
+    li.setAttribute("class", "featuredNavLi")
+    let a = document.createElement("a")
+    a.setAttribute("class", "featuredNavLiLink")
+    a.setAttribute("href", "productsDisplay.html")
+    a.textContent = item.name
+    li.addEventListener("click", function (event) {
+      event.preventDefault()
+      var pageName = localStorage.getItem("productsPageName") || ""
+      localStorage.setItem("productsPageName", item.name)
+      console.log(item.name)
+      window.location.href = "productsDisplay.html"
+    })
+    li.append(a)
+    document.getElementById("listCat").append(li)
+    let card = document.createElement("div")
+    card.setAttribute("class", "popularCategoriesItem")
+    card.addEventListener("click", function (event) {
+      event.preventDefault()
+      var pageName = localStorage.getItem("productsPageName") || ""
+      localStorage.setItem("productsPageName", item.name)
+      console.log(item.name)
+      window.location.href = "productsDisplay.html"
+    })
+    let imgDiv = document.createElement("div")
+    imgDiv.setAttribute("class", "popularCategoriesItemImgDiv")
+    let img = document.createElement("img")
+    img.setAttribute("src", item.imgUrl)
+    img.setAttribute("class", "popularCategoriesItemImg")
+    img.width = "400"
+    img.height = "400"
+    let titleDIv = document.createElement("div")
+    titleDIv.setAttribute("class", "popularCategoriesItemText")
+    titleDIv.textContent = item.name
+    imgDiv.append(img)
+    card.append(imgDiv, titleDIv)
+    document.getElementById("catWithImg").append(card)
+  })
+}
 var appendMoreCategories = (data) => {
-  console.log(data);
+  console.log(data)
   data.forEach((item) => {
-    let card = document.createElement("div");
-    card.setAttribute("href", "");
-    card.setAttribute("class", "categoryCard");
-    let a = document.createElement("a");
-    a.setAttribute("href", "");
-    a.setAttribute("class", "categoryCardLink");
-    let imgDiv = document.createElement("div");
-    imgDiv.setAttribute("class", "categoryCardImgDiv");
-    let img = document.createElement("img");
-    img.setAttribute("src", item.imgUrl);
-    img.setAttribute("class", "categoryCardImg");
-    let titleDIv = document.createElement("div");
-    titleDIv.setAttribute("class", "categoryCardTitleDiv");
-    let title = document.createElement("h3");
-    title.setAttribute("class", "title-xs categoryCardTitle");
-    title.textContent = item.name;
-    imgDiv.append(img);
-    titleDIv.append(title);
-    a.append(imgDiv, titleDIv);
-    card.append(a);
-    document.getElementById("moreCat").append(card);
-  });
-};
+    let card = document.createElement("div")
+    card.setAttribute("class", "categoryCard")
+    let a = document.createElement("a")
+    a.setAttribute("href", "productsDisplay.html")
+    a.setAttribute("class", "categoryCardLink")
+    let imgDiv = document.createElement("div")
+    imgDiv.setAttribute("class", "categoryCardImgDiv")
+    let img = document.createElement("img")
+    img.setAttribute("src", item.imgUrl)
+    img.setAttribute("class", "categoryCardImg")
+    let titleDIv = document.createElement("div")
+    titleDIv.setAttribute("class", "categoryCardTitleDiv")
+    let title = document.createElement("h3")
+    title.setAttribute("class", "title-xs categoryCardTitle")
+    title.textContent = item.name
+    imgDiv.append(img)
+    titleDIv.append(title)
+    a.append(imgDiv, titleDIv)
+    card.append(a)
+    document.getElementById("moreCat").append(card)
+  })
+}
+let productsPage = (event) => {}
---
+++ Script/productsDisplay.js
@@ -0,0 +1,134 @@
+var pageName = localStorage.getItem("productsPageName") || ""
+var nn = pageName
+console.log(nn)
+document.getElementById("headingResult").textContent = nn
+fetch(`https://overstockapi.herokuapp.com/products/category=${nn}`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((response) => {
+    var data = response
+    console.log(data)
+    appendCard(data)
+  })
+fetch(`https://overstockapi.herokuapp.com/products/mainSubCategory=${nn}`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((response) => {
+    var data = response
+    console.log(data)
+    appendCard(data)
+  })
+
+var appendCard = (data) => {
+  data.forEach((item) => {
+    let productCardContainer = document.createElement("div")
+    productCardContainer.setAttribute("class", "productCardContainer")
+    let div = document.createElement("div")
+    let productCardLink = document.createElement("a")
+    productCardLink.setAttribute("class", "productCardLink")
+    productCardLink.setAttribute("href", "")
+    productCardLink.setAttribute("target", "_blank")
+    let productCard = document.createElement("div")
+    productCard.setAttribute("class", "productCard")
+    let productCardFrontMain = document.createElement("div")
+    productCardFrontMain.setAttribute("class", "productCardFrontMain")
+    let productCardInfoBannerContainer = document.createElement("div")
+    productCardInfoBannerContainer.setAttribute(
+      "class",
+      "productCardInfoBannerContainer"
+    )
+    let tapcontainer = document.createElement("div")
+    tapcontainer.setAttribute("class", "tap-container")
+    let wishlistFavContainerNew = document.createElement("div")
+    wishlistFavContainerNew.setAttribute("class", "wishlistFavContainerNew")
+    let wishlistHeart = document.createElement("img")
+    wishlistHeart.setAttribute(
+      "src",
+      "https://i.postimg.cc/W3cWM2Xg/Add-to-Wishlist.png"
+    )
+    wishlistHeart.setAttribute("class", "wishlistHeart")
+    let wishlistCount = document.createElement("div")
+    wishlistCount.setAttribute("class", "wishlistCount")
+    wishlistCount.innerText = `${Math.floor(
+      Math.random() * (110 - 250) + 250
+    )} K`
+    wishlistFavContainerNew.append(wishlistHeart, wishlistCount)
+    tapcontainer.append(wishlistFavContainerNew)
+    let div2 = document.createElement("div")
+    productCardInfoBannerContainer.append(div2, tapcontainer)
+    let productCardFrontImageContainer = document.createElement("div")
+    productCardFrontImageContainer.setAttribute(
+      "class",
+      "productCardFrontImageContainer"
+    )
+    let productCardFrontImage = document.createElement("img")
+    productCardFrontImage.setAttribute("class", "productCardFrontImage")
+    productCardFrontImage.width = "320"
+    productCardFrontImage.height = "320"
+    productCardFrontImage.setAttribute("src", item.imageURL)
+    productCardFrontImageContainer.append(productCardFrontImage)
+    productCardFrontMain.append(
+      productCardInfoBannerContainer,
+      productCardFrontImageContainer
+    )
+    let productCardInfoBarContainerWithTitle = document.createElement("div")
+    productCardInfoBarContainerWithTitle.setAttribute(
+      "class",
+      "productCardInfoBarContainerWithTitle"
+    )
+    let priceRatingTitle = document.createElement("div")
+    priceRatingTitle.setAttribute("class", "priceRatingTitle")
+    let productPricing = document.createElement("div")
+    productPricing.setAttribute("class", "productPricing _2DfMCJ")
+    let productPricingCurrent = document.createElement("div")
+    productPricingCurrent.setAttribute(
+      "class",
+      "productPricingCurrent _3MUO5L wzKvpP"
+    )
+    productPricingCurrent.textContent = `INR ${item.price}`
+    productPricing.append(productPricingCurrent)
+    let ratingsWrapper = document.createElement("div")
+    ratingsWrapper.setAttribute("class", "ratingsWrapper")
+    let rate2 = 0
+    let rate = Math.floor(item.rating)
+    rate2 = item.rating - Math.floor(item.rating)
+    console.log(rate2)
+    for (var i = 0; i < rate; i++) {
+      var ratingsStars = document.createElement("img")
+      ratingsStars.setAttribute(
+        "src",
+        "https://i.postimg.cc/43bGzWp3/svgexport-16.png"
+      )
+      ratingsStars.setAttribute("class", "ratingsStars")
+      ratingsWrapper.append(ratingsStars)
+    }
+    if (rate2 !== 0) {
+      var ratingsStars2 = document.createElement("img")
+      ratingsStars2.setAttribute(
+        "src",
+        "https://i.postimg.cc/kM2dXQfL/svgexport-18.png"
+      )
+      ratingsStars2.setAttribute("class", "ratingsStars")
+      ratingsWrapper.append(ratingsStars2)
+    }
+    let productCardFrontTitle = document.createElement("p")
+    productCardFrontTitle.setAttribute("class", "productCardFrontTitle")
+    productCardFrontTitle.textContent = item.name
+    priceRatingTitle.append(
+      productPricing,
+      ratingsWrapper,
+      productCardFrontTitle
+    )
+    productCardInfoBarContainerWithTitle.append(priceRatingTitle)
+    productCard.append(
+      productCardFrontMain,
+      productCardInfoBarContainerWithTitle
+    )
+    productCardLink.append(productCard)
+    div.append(productCardLink)
+    productCardContainer.append(div)
+    document.getElementById("displayCard").append(productCardContainer)
+  })
+}
